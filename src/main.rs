daab89d510724ed3e9c0e20c23ef51884b1cc09f
A-kua <732102386@qq.com>
2024-03-30 00:30:40 +0800

    implement image handler use actix-files

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,25 @@ use crate::route::{article, css, image, js};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let guide_scope = web::scope("/")
+        let guide_scope = web::scope("")
             .guard(guard::Host("127.0.0.1"))
-            .route("", web::get().to(site::guide::index));
+            .route("/", web::get().to(site::guide::index));
 
-        let akua_scope = web::scope("/")
+        let akua_scope = web::scope("")
             .guard(guard::Host("akua.fan"))
-            .route("", web::get().to(site::akua::index));
+            .route("/", web::get().to(site::akua::index));
 
-        let polite_scope = web::scope("/")
+        let polite_scope = web::scope("")
             .guard(guard::Host("polite.cat"))
-            .route("", web::get().to(site::polite::index));
+            .route("/", web::get().to(site::polite::index));
 
-        let blog_scope = web::scope("/")
-            .guard(guard::Host("blog.akua.fan"))
-            .route("", web::get().to(site::blog::index))
-            .service(web::scope("/article")
+        let blog_scope = web::scope("")
+            // .guard(guard::Host("blog.akua.fan"))
+            // .guard(guard::Host("localhost"))
+            .route("/", web::get().to(site::blog::index))
+            .service(web::scope("article")
                 .service(article))
-            .service(web::scope("/static")
+            .service(web::scope("static")
                 .service(image)
                 .service(css)
                 .service(js));
@@ -37,9 +38,9 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(BlogState {
                 visit_count: Arc::new(AtomicUsize::new(0)),
             }))
-            .service(guide_scope)
-            .service(akua_scope)
-            .service(polite_scope)
+            // .service(guide_scope)
+            // .service(akua_scope)
+            // .service(polite_scope)
             .service(blog_scope)
     })
         .bind(("0.0.0.0", 8080))?
--- src/route.rs
+++ src/route.rs
@@ -1,7 +1,8 @@
-use actix_web::{Error};
+use actix_web::{Error, HttpRequest};
 use futures::{future::ok, stream::once};
 use actix_web::{get, HttpResponse, Responder, web};
 use crate::bean::{Article};
+use actix_files::{Files, NamedFile};
 
 #[get("/{name}/{section}")]
 async fn article(path: web::Path<Article>) -> impl Responder {
@@ -9,12 +10,21 @@ async fn article(path: web::Path<Article>) -> impl Responder {
 }
 
 #[get("/image/{name}")]
-async fn image(path: web::Path<(String)>) -> HttpResponse {
-    let bytes = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
+async fn image(req: HttpRequest,name: web::Path<(String)>) -> HttpResponse {
+    let file_path = std::path::PathBuf::from(std::env::var("BLOG_STATIC").unwrap())
+        .as_path()
+        .join("static-image")
+        .join(&name.into_inner());
 
-    HttpResponse::Ok()
-        .content_type("image/png")
-        .streaming(bytes)
+    match NamedFile::open_async(file_path).await {
+        Ok(file) => {
+            file.into_response(&req)
+        },
+        Err(_) => {
+            HttpResponse::NotFound()
+                .body("File not found")
+        },
+    }
 }
 
 #[get("/css/{name}")]
